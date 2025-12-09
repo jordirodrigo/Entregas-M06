672c2b14083de46302f2805e982ebf19e4d7cc90
jordi_rodrigo <jordi.rodrigo.7e8@itb.cat>
2025-12-09 19:44:41 +0100

    asfdgfhgjh

---
+++ DOM/Ex1.js
@@ -0,0 +1,11 @@
+let titulo = document.getElementById("titulo");
+titulo.textContent = "¡Bienvenido al DOM con JavaScript!";
+
+document.getElementById("paragrafo").textContent = "Este es un párrafo agregado dinámicamente usando JavaScript.";
+
+document.getElementById("Mascota").src = "4.png";
+
+let newArticle = document.createElement("article");
+newArticle.innerHTML = "<h2>Artículo Dinámico</h2><p>Este artículo fue creado y agregado al DOM usando JavaScript.</p>";
+document.body.appendChild(newArticle);
+document.getElementById("main").appendChild(newArticle);
