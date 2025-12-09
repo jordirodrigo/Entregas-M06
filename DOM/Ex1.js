19d1115fc71a627a16b51d0290a2b7918f1fe5dd
jordi_rodrigo <jordi.rodrigo.7e8@itb.cat>
2025-12-09 20:01:14 +0100

    safdasgbcxnbvm,m

--- DOM/Ex1.js
+++ DOM/Ex1.js
@@ -9,3 +9,4 @@ let newArticle = document.createElement("article");
 newArticle.innerHTML = "<h2>Artículo Dinámico</h2><p>Este artículo fue creado y agregado al DOM usando JavaScript.</p>";
 document.body.appendChild(newArticle);
 document.getElementById("main").appendChild(newArticle);
+ 
