7c144aefbb77e7c2d9ac0107b95319c6d04c0d8d
jordi_rodrigo <jordi.rodrigo.7e8@itb.cat>
2026-01-14 16:22:54 +0100

    asdfghj

--- 3D/Prueba guitarra/three.js
+++ 3D/Prueba guitarra/three.js
@@ -24,58 +24,49 @@ const dirLight = new THREE.DirectionalLight(0xffffff, 1);
 dirLight.position.set(5, 5, 5);
 scene.add(dirLight);
 
-// --- C. CARGAR MODELO ---
-const loader = new GLTFLoader();
-let model;
-
-loader.load(
-    'Models/12345.glb',
-    (gltf) => {
-        model = gltf.scene;
-        model.scale.set(0.5, 0.5, 0.5);
-        model.position.set(20, 0, 0);
-
-        // --- SOLUCIÓN TRANSPARENCIA Y ORDEN DE DIBUJADO ---
-        let order = 0;
-        model.traverse((child) => {
-            if (child.isMesh) {
-                child.renderOrder = order++;      // fuerza el orden de dibujado
-                const mat = child.material;
-
-                if (mat.transparent) {
-                    mat.side = THREE.DoubleSide; // dibuja ambos lados
-                    mat.depthWrite = false;      // evita que se tapen entre sí
-                } else {
-                    mat.side = THREE.FrontSide; // dibuja solo el frente
-                    mat.depthWrite = true;
-                }
-
-                // opcional: si hay recortes en textura
-                // mat.alphaTest = 0.01;
-            }
-        });
-
-        scene.add(model);
-        console.log('Modelo cargado correctamente');
-    },
-    (xhr) => console.log(`Cargando modelo: ${(xhr.loaded / xhr.total * 100).toFixed(2)}%`),
-    (error) => console.error('Error cargando el modelo:', error)
-
-);
 // base cuerpo cilíndrico
 const geometry = new THREE.CylinderGeometry(4, 4, 10, 11);
-const material = new THREE.MeshStandardMaterial({color: 0x2233FF, emissive: 0x112244
+const material = new THREE.MeshStandardMaterial({
+    color: 0x2233FF, emissive: 0x112244
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// base cuerpo cilíndrico
-const geometry2 = new THREE.CylinderGeometry(4, 4, 10, 11);
+// brazo1  cilíndrico
+const geometry2 = new THREE.CylinderGeometry(1, 1, 10, 11);
+geometry2.translate(0, -5, 0);
 const material2 = new THREE.MeshStandardMaterial({
-    
+    color: 0x00FF00, emissive: 0x112244
+
 });
+
 const cube2 = new THREE.Mesh(geometry2, material2);
 scene.add(cube2);
+// brazo2  cilíndrico
+const geometry3 = new THREE.CylinderGeometry(1, 1, 10, 11);
+geometry3.translate(0, -5, 0);
+const material3 = new THREE.MeshStandardMaterial({
+    color: 0x00FF00, emissive: 0x112244
+
+});
+const cube3 = new THREE.Mesh(geometry3, material3);
+scene.add(cube3);
+// pierna1  cilíndrico
+const geometry4 = new THREE.CylinderGeometry(1.5, 1.5, 10, 11);
+const material4 = new THREE.MeshStandardMaterial({
+    color: 0x00FF00, emissive: 0x112244
+
+});
+const cube4 = new THREE.Mesh(geometry4, material4);
+scene.add(cube4);
+// pierna2  cilíndrico
+const geometry5 = new THREE.CylinderGeometry(1.5, 1.5, 10, 11);
+const material5 = new THREE.MeshStandardMaterial({
+    color: 0x00FF00, emissive: 0x112244
+});
+const cube5 = new THREE.Mesh(geometry5, material5);
+scene.add(cube5);
+
 
 
 
@@ -91,6 +82,27 @@ function animate() {
     //if (model) {
     // model.rotation.y += 0.005; // rotación automática opcional
     //}
+
+    const t = Date.now() * 0.001;
+
+
+    cube2.position.x = 5;
+    cube2.position.y = 5;
+    cube2.rotation.x = -Math.sin(t) * 0.3;
+
+    cube3.position.x = -5;
+    cube3.position.y = 5;
+    cube3.rotation.x = Math.sin(t) * 0.3;
+
+    cube4.position.x = 2;
+    cube4.position.y = -10;
+    cube4.rotation.y = 0;
+
+    cube5.position.x = -2;
+    cube5.position.y = -10;
+    cube5.rotation.y = 0;
+
+
 
     controls.update();
     renderer.render(scene, camera);
